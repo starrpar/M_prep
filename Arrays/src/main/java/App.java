c7b304ef439ed005c9a867d5719213180cc2a44f
Starr <starrparker@live.com>
2024-03-20 09:16:13 -0700

    initial upload

--- Arrays/src/main/java/App.java
+++ Arrays/src/main/java/App.java
@@ -1,9 +1,18 @@
+import main.java.SupportFunctions;
 
 public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        System.out.println("Hello world!");
 
+        // RemoveElement fcns = new RemoveElement();
+
+        /*
+         * int[] arr = { 1, 2, 2, 3, 4, 5, 5, 1 };
+         * int val = 1;
+         * int count = fcns.removeElement(arr, val);
+         * System.out.println(count);
+         */
     }
 }
---
+++ Arrays/src/main/java/RemoveElement.java
@@ -0,0 +1,13 @@
+
+public class RemoveElement {
+
+    public int removeElement(int[] arr, int val) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != val) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
