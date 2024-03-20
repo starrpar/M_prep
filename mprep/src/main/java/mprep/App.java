1acd78afec3314dd013d041973f9b8fea9c272ab
Starr <starrparker@live.com>
2024-03-20 12:59:37 -0700

    building out array methods for practice

--- mprep/src/main/java/mprep/App.java
+++ mprep/src/main/java/mprep/App.java
@@ -4,17 +4,29 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
         ArrayMethods fcns = new ArrayMethods();
 
-        int arrayLength = 50;
+        int arrayLength = 4000;
         int bound = 10;
         int[] newIntArr = fcns.createRandomIntArray(arrayLength, bound);
+        char[] newCharArr = fcns.createRandomCharArray(arrayLength);
         int[] arr1 = newIntArr.clone();
         int[] arr2 = newIntArr.clone();
+        int intArrLen = newIntArr.length;
+        int charArrLen = newCharArr.length;
+        Object[] arr3 = new Object[intArrLen];
+        for (int i = 0; i < intArrLen; i++) {
+            arr3[i] = (Object) newIntArr[i];
+        }
+        Object[] arr4 = new Object[charArrLen];
+        for (int i = 0; i < charArrLen; i++) {
+            arr4[i] = (Object) newCharArr[i];
+            // System.out.print(arr4[i]);
+        }
+
         // int[] arr1 = { 1, 2, 2, 3, 4, 5, 5, 1, 1 };
         // int[] arr2 = { 1, 2, 2, 3, 4, 5, 5, 1, 1 };
+
         int val = 8;
         System.out.println("\nFirst - doesn't fix: ");
         int count = fcns.removeElementInPlace1(arr1, val);
@@ -22,5 +34,33 @@ public class App {
         System.out.println("\nSecond - does fix: ");
         count = fcns.removeElementInPlace2(arr2, val);
         System.out.println("\n" + count);
+        System.out.println("\n" + fcns.removeDuplicates(arr3));
+        System.out.println("\n" + fcns.removeDuplicates(arr4));
+
+        int numIntOccurrences = fcns.countFrequencyOfElement(arr3, (Object) 3);
+        System.out.println("\nFrequency of number 3: " + numIntOccurrences);
+        int expectedOccurrences = arrayLength / bound;
+        System.out.println("number of expected \"random\" occurrences in array of int of length: " + arrayLength
+                + " is: " + expectedOccurrences);
+        if (numIntOccurrences > expectedOccurrences) {
+            System.out.println(
+                    "...so value is more common than expected, so \'randomeness\' is more liberal in this case");
+        } else {
+            System.out.println(
+                    "...so value is less common than expected, so \'randomeness\' is more conservative in this case");
+        }
+
+        int numCharOccurrences = fcns.countFrequencyOfElement(arr4, (Object) 'c');
+        System.out.println("\nFrequency of character \'c\': " + numCharOccurrences);
+        expectedOccurrences = arrayLength / 52;
+        System.out.println("number of expected \"random\" occurrences in array of char of length: " + arrayLength
+                + " is: " + expectedOccurrences);
+        if (numCharOccurrences > expectedOccurrences) {
+            System.out.println(
+                    "...so value is more common than expected, so \'randomeness\' is more liberal in this case");
+        } else {
+            System.out.println(
+                    "...so value is less common than expected, so \'randomeness\' is more conservative in this case");
+        }
     }
 }
--- mprep/src/main/java/mprep/ArrayMethods.java
+++ mprep/src/main/java/mprep/ArrayMethods.java
@@ -6,6 +6,41 @@ import java.util.Random;
 
 public class ArrayMethods {
 
+    Object findMinElement(Object[] arr) {
+        // this could be by numeric value only, or using ascii as integer values
+        // equivalent to findMaxElement()
+
+    }
+
+    int removeDuplicates(Object[] arr) {
+        int count = 0;
+        List<Object> alreadyPresentList = new ArrayList<>();
+        List<Object> tmpArray = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!alreadyPresentList.contains(arr[i])) {
+                tmpArray.add(arr[i]);
+                count++;
+                alreadyPresentList.add(arr[i]);
+            }
+        }
+        Object[] newArr = new Object[count];
+
+        for (int i = 0; i < count; i++) {
+            newArr[i] = tmpArray.get(i);
+        }
+        arr = newArr;
+        // print out to confirm/debugging
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) {
+                System.out.print(arr[i] + ",");
+            } else {
+                System.out.print(arr[i]);
+            }
+        }
+        return count;
+    }
+
     // package-private access (default)
     int[] createRandomIntArray(int length, int bound) {
         Random rdm = new Random();
@@ -14,8 +49,44 @@ public class ArrayMethods {
         for (int i = 0; i < length; i++) {
             returnArray[i] = rdm.nextInt(bound);
         }
+        System.out.println("\nLength of final int array: " + returnArray.length);
 
         return returnArray;
+    }
+
+    // package-private access (default)
+    char[] createRandomCharArray(int length) {
+        Random rdm = new Random();
+        char[] returnArray = new char[length];
+
+        // NOTE: ascii - A = 65; Z = 90; a = 97; z = 122
+        int decider = 0;
+        int num = 0;
+        int val = 0;
+
+        for (int i = 0; i < length; i++) {
+            decider = rdm.nextInt(2); // either 0 or 1
+            if (decider == 0) {
+                val = (int) 'A';
+            } else {
+                val = (int) 'a';
+            }
+            returnArray[i] = (char) (val + rdm.nextInt(26));
+        }
+        System.out.println("\nLength of final char array: " + returnArray.length);
+
+        return returnArray;
+    }
+
+    public int countFrequencyOfElement(Object[] arr, Object val) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val) {
+                arr[count++] = arr[i];
+            }
+        }
+
+        return count;
     }
 
     public int removeElementInPlace1(int[] arr, int val) {
@@ -29,6 +100,7 @@ public class ArrayMethods {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+        System.out.print("\nLength of final array: " + arr.length);
         return count;
     }
 
@@ -52,6 +124,7 @@ public class ArrayMethods {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+        System.out.print("\nLength of final array: " + arr.length);
         return count;
     }
 }
