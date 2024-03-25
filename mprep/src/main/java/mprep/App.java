03e8b428dff5bc9f6f9f0f640e86319c5b7a123b
Starr <starrparker@live.com>
2024-03-24 22:16:15 -0700

    minor cleanup

--- mprep/src/main/java/mprep/App.java
+++ mprep/src/main/java/mprep/App.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import mprep.DynamicProgrammingMethods.NQueensProblem;
-
 public class App {
 
     public static void main(String[] args) {
