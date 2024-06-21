056799ddb820c478fe9a7bb0b1e201e56da82ba2
Starr <starrparker@live.com>
2024-06-21 14:26:35 -0700

    added answers for test strings

--- mprep/src/cpp/AlgoExp.cpp
+++ mprep/src/cpp/AlgoExp.cpp
@@ -24,6 +24,67 @@ int main(int argc, char *argv[])
     cout << runLengthEncoding(testStr9);
 }
 
+// expected results of below...
+//  ---------- Test Case 1 ----------
+//  AAAAAAAAAAAAABBCCCCDD
+//  9A4A2B4C2D
+
+// ---------- Test Case 2 ----------
+// aA
+// 1a1A
+
+// ---------- Test Case 3 ----------
+// 122333
+// 112233
+
+// ---------- Test Case 4 ----------
+// ************^^^^^^^$$$$$$%%%%%%%!!!!!!AAAAAAAAAAAAAAAAAAAA
+// 9*3*7^6$7%6!9A9A2A
+
+// ---------- Test Case 5 ----------
+// aAaAaaaaaAaaaAAAABbbbBBBB
+// 1a1A1a1A5a1A3a4A1B3b4B
+
+// ---------- Test Case 6 ----------
+
+// 9 9 8
+
+// ---------- Test Case 7 ----------
+// 1  222 333    444  555
+// 112 321 334 342 35
+
+// ---------- Test Case 8 ----------
+// 1A2BB3CCC4DDDD
+// 111A122B133C144D
+
+// ---------- Test Case 9 ----------
+// ........______=========AAAA   AAABBBB   BBB
+// 8.6_9=4A3 3A4B3 3B
+
+// ---------- Test Case 10 ----------
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// 9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a1a
+
+// ---------- Test Case 11 ----------
+//         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// 8 9a9a9a9a9a4a
+
+// ---------- Test Case 12 ----------
+
+// 1
+
+// ---------- Test Case 13 ----------
+// [(aaaaaaa,bbbbbbb,ccccc,dddddd)]
+// 1[1(7a1,7b1,5c1,6d1)1]
+
+// ---------- Test Case 14 ----------
+// ;;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s
+// 9;3;9'9'2'111273524142311s
+
+// ---------- Test Case 15 ----------
+// AAAAAAAAAAAAABBCCCCDDDDDDDDDDD
+// 9A4A2B4C9D2D
+
 string runLengthEncoding(string str)
 {
     // All characters types are permitted (so spaces, special chars,
