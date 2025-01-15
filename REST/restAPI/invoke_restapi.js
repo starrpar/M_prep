f2a7f7bb8ad8f9545f5fc360e7e20e519ea748ad
starrpar <starrparker@live.com>
2025-01-15 12:23:33 -0800

    minor updates

--- REST/restAPI/invoke_restapi.js
+++
@@ -1,4 +0,0 @@
-   fetch('http://localhost:8080/hello')
-       .then(response => response.text())
-       .then(data => console.log(data))
-       .catch(error => console.error(error));
--- REST/restAPI/src/main/java/com/parker/app/restAPI/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.parker.app.restAPI.Repo;
-
-import com.parker.app.restAPI.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
